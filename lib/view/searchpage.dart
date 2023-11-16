b91d37631d98b5c390ebffa2e15d832dae781433
Kittinan Thaimnarong <kittinan.t@mbkkkittinant.aware.local>
2023-11-16 10:27:20 +0700

    update:code using api and show data

--- lib/view/searchpage.dart
+++ lib/view/searchpage.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
-
 import '../model/list_info.dart';
 import '../model/name_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -13,14 +14,35 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   // ignore: non_constant_identifier_names
-  List<NameModel> display_list = List.from(search_name_list);
-  void updateList(String value) {
-    setState(() {
-      display_list = search_name_list
-          .where((element) =>
-              element.firstname!.toLowerCase().contains(value.toLowerCase()))
-          .toList();
-    });
+  // List<NameModel> display_list = List.from(search_name_list);
+  final myController = TextEditingController();
+  Map? mapResponse;
+  Map? dataResonse;
+  List? listResponse;
+
+  Future updateListFromApi(String name) async {
+    http.Response response;
+    // response = await http.get(Uri.https('www.melivecode.com', '/api/users/$name'));
+    response = await http.get(Uri.parse('https://reqres.in/api/users?page=$name'));
+    if (response.statusCode == 200) {
+      setState(() {
+        mapResponse = json.decode(response.body);
+        listResponse = mapResponse?['data'];
+      });
+    }
+  }
+
+  // void updateList(String value) {
+  //   setState(() {
+  //     display_list =
+  //         search_name_list.where((element) => element.firstname!.toLowerCase().contains(value.toLowerCase())).toList();
+  //   });
+  // }
+  @override
+  void initState() {
+    updateListFromApi('1');
+
+    super.initState();
   }
 
   @override
@@ -39,47 +61,47 @@ class _SearchPageState extends State<SearchPage> {
           children: [
             Text(
               'Search for name',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold),
+              style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
             ),
             SizedBox(
               height: 20,
             ),
             TextField(
-              onChanged: (value) => updateList(value),
+              controller: myController,
+              // onChanged: (value) => updateList(value),
               style: TextStyle(color: Colors.white),
               decoration: InputDecoration(
                   filled: true,
                   fillColor: Color(0xFF302360),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide.none),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(8), borderSide: BorderSide.none),
                   hintText: 'ex: Kittinan',
                   prefixIcon: Icon(Icons.search),
                   prefixIconColor: Colors.purple.shade900),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await updateListFromApi(myController.text);
+              },
+              child: Text('Search'),
             ),
             SizedBox(
               height: 20,
             ),
             Expanded(
                 child: ListView.builder(
-                    itemCount: display_list.length,
+                    itemCount: listResponse?.length,
                     itemBuilder: (context, index) => ListTile(
                           contentPadding: EdgeInsets.all(8),
                           title: Text(
-                            '${display_list[index].firstname}',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
+                            '${listResponse?[index]['first_name'].toString()}',
+                            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
                           ),
                           subtitle: Text(
-                            '${display_list[index].lastname}',
+                            '${listResponse?[index]['last_name'].toString()}',
                             style: TextStyle(color: Colors.white60),
                           ),
                           trailing: Text(
-                            '${display_list[index].age} year',
+                            '${listResponse?[index]['email'].toString()}',
                             style: TextStyle(color: Colors.amber),
                           ),
                         )))
